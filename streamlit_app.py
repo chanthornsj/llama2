3ed3e3c9f231079cae53f485d6b984290364b6b7
chanthornsj <120350244+chanthornsj@users.noreply.github.com>
2023-08-10 11:53:06 +0700

    Update some response message

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="🦙💬 PHILLIP Chatbot (LLAMA2)")
 
 # Replicate Credentials
 with st.sidebar:
@@ -22,7 +22,7 @@ os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome to Phillip Bank, how may I assist you today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
@@ -30,7 +30,7 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Phillip Assistant is here, How may I assist you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
