a75e0958d90541fb690b2121b80b6e69b500b019
chanthornsj <120350244+chanthornsj@users.noreply.github.com>
2023-08-11 10:56:40 +0700

    disable sidebar.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="PHILLIP Chatbot (LLAMA2)")
 #    else:
 #        replicate_api = st.text_input('Enter Replicate API token:', type='password')
 #        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+#            st.warning('Please enter your credentials!', icon='⚠️')
 #        else:
 #            st.success('Proceed to entering your prompt message!' + replicate_api, icon='👉')
 #    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
