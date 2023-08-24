895b1f0c40318e1eba3758762f37caba1ff99add
chanthornsj <120350244+chanthornsj@users.noreply.github.com>
2023-08-24 13:55:57 +0700

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-with st.echo():
-  st.text("This is some text")
-  factorial_5 = 5 * 4 * 3 * 2 * 1
-  st.write("The factorial of 5 is", factorial_5)
+st.text("This is some text")
+factorial_5 = 5 * 4 * 3 * 2 * 1
+st.write("The factorial of 5 is", factorial_5)
